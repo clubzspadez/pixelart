532f187c50938aa44e285c8a4db9dc3071783496
Jonathan Rodriguez <clubzspadez@gmail.com>
2017-09-06 15:37:11 -0400

    init repos

---
+++ designs.js
@@ -0,0 +1,28 @@
+// Select color input
+
+
+
+// Select size input
+
+
+
+
+// When size is submitted by the user, call makeGrid()
+
+$('submit').on('click', makeGrid());
+
+function makeGrid() {
+
+// Your code goes here!
+
+//Step 1.
+
+//Step 2.
+
+//Step 3.
+
+//Step 4.
+
+//Step 4.
+
+}
