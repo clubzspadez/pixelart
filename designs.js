17fbf9d93bcd35945beaa0a183e79b90b0d00f26
Jonathan Rodriguez <clubzspadez@gmail.com>
2017-09-08 18:56:23 -0400

    Finished proj

--- designs.js
+++ designs.js
@@ -1,28 +1,39 @@
-// Select color input
 
 
+//Grab DOM elements
+var canvas = $('#pixel_canvas');
+const widthInput = $('#input_width');
+const heightInput = $('#input_height');
 
-// Select size input
+//eventlistener on submit for input
+$('#sizePicker').on('submit',function (){
+	//create variables to store those values
+	let widthIn = $('#input_width').val();
+	let heightIn = $('#input_height').val();
+	//Remove all child nodes from table
+	$('table').empty();
+	//call function and pass variables
+	makeGrid(widthIn, heightIn);
+	//prevents refresh
+	event.preventDefault();
 
+});
 
+//makeGrid function with parameters passing width and height
+function makeGrid(width, height) {
 
-
-// When size is submitted by the user, call makeGrid()
-
-$('submit').on('click', makeGrid());
-
-function makeGrid() {
-
-// Your code goes here!
-
-//Step 1.
-
-//Step 2.
-
-//Step 3.
-
-//Step 4.
-
-//Step 4.
-
+// loop through height
+for(var x = 0; x < height; x++){
+	$('table').append("<tr></tr>");
+	}
+// loop  through width
+for( var y = 0; y < width; y++){
+		$('tr').append("<td></td>");
+		}
 }
+
+//Place color in table data
+$('table').on('click', 'td', function() {
+    const colour = $("#colorPicker").val();
+    $(this).css('backgroundColor', colour);
+});
